0ba6549714a7086d77eb80c9571b10579a9fd29c
workonlly <workonlly335@gmail.com>
2026-06-28 17:57:03 +0530

    sdcsd

--- app/test3.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({path: '../.env'});
-const { drive } = require('./db/doogledrive');
-drive.files.list({ q: `'${process.env.NEXT_PUBLIC_LITERACY_WORKS}' in parents and trashed = false`, fields: 'files(id, name, webViewLink, mimeType)' })
-  .then(r => console.log("Works:", r.data.files))
-  .catch(e => console.log(e));
--- app/test4.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config({path: '../.env'});
-const { drive } = require('./db/doogledrive');
-drive.files.get({ fileId: '1We_f0E10B_XRNJB-nCDCl44wlbKGrKx8', alt: 'media' }, { responseType: 'json' })
-  .then(r => console.log("JSON:", JSON.stringify(r.data, null, 2)))
-  .catch(e => console.log(e));
